683761c068f73511b978525db86d0efdfa514926
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-24 15:39:00 +0900

    got pause icons to toggle

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ function togglePlay() {
   }
 }
 
+function updateButton() {
+  const icon = this.paused ? '►' : '| |';
+  toggle.textContent = icon;
+}
+
+
 // hook up the event listeners
 
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
