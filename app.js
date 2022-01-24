3f495b0010795d2e63ae22b66034abe610a1b542
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-24 15:28:24 +0900

    toggle method works

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
+console.log(video);
 
 // build functions
+
+function togglePlay() {
+  if (video.paused) {// paused is a property that lives in 'video' element
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
 // hook up the event listeners
